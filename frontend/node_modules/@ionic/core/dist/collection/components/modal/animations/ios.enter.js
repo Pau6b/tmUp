5c78d897b9fa1fef88b05f983172c69016188184
Pau6b <pau.murciano1@gmail.com>
2020-03-10 11:46:47 +0100

    Reorder folders

---
+++ frontend/node_modules/@ionic/core/dist/collection/components/modal/animations/ios.enter.js
@@ -0,0 +1,44 @@
+import { createAnimation } from '../../../utils/animation/animation';
+import { SwipeToCloseDefaults } from '../gestures/swipe-to-close';
+/**
+ * iOS Modal Enter Animation for the Card presentation style
+ */
+export const iosEnterAnimation = (baseEl, presentingEl) => {
+    // The top translate Y for the presenting element
+    const backdropAnimation = createAnimation()
+        .addElement(baseEl.querySelector('ion-backdrop'))
+        .fromTo('opacity', 0.01, 'var(--backdrop-opacity)');
+    const wrapperAnimation = createAnimation()
+        .addElement(baseEl.querySelector('.modal-wrapper'))
+        .beforeStyles({ 'opacity': 1 })
+        .fromTo('transform', 'translateY(100%)', 'translateY(0%)');
+    const baseAnimation = createAnimation()
+        .addElement(baseEl)
+        .easing('cubic-bezier(0.32,0.72,0,1)')
+        .duration(500)
+        .beforeAddClass('show-modal')
+        .addAnimation([backdropAnimation, wrapperAnimation]);
+    if (presentingEl) {
+        const modalTransform = (presentingEl.tagName === 'ION-MODAL' && presentingEl.presentingElement !== undefined) ? '-10px' : 'max(30px, var(--ion-safe-area-top))';
+        const bodyEl = document.body;
+        const toPresentingScale = SwipeToCloseDefaults.MIN_PRESENTING_SCALE;
+        const finalTransform = `translateY(${modalTransform}) scale(${toPresentingScale})`;
+        const presentingAnimation = createAnimation()
+            .beforeStyles({
+            'transform': 'translateY(0)',
+            'transform-origin': 'top center',
+            'overflow': 'hidden'
+        })
+            .afterStyles({
+            'transform': finalTransform
+        })
+            .beforeAddWrite(() => bodyEl.style.setProperty('background-color', 'black'))
+            .addElement(presentingEl)
+            .keyframes([
+            { offset: 0, filter: 'contrast(1)', transform: 'translateY(0px) scale(1)', borderRadius: '0px' },
+            { offset: 1, filter: 'contrast(0.85)', transform: finalTransform, borderRadius: '10px 10px 0 0' }
+        ]);
+        baseAnimation.addAnimation(presentingAnimation);
+    }
+    return baseAnimation;
+};
---
+++ frontend/node_modules/@ionic/core/dist/collection/components/modal/animations/ios.leave.js
@@ -0,0 +1,45 @@
+import { createAnimation } from '../../../utils/animation/animation';
+import { SwipeToCloseDefaults } from '../gestures/swipe-to-close';
+/**
+ * iOS Modal Leave Animation
+ */
+export const iosLeaveAnimation = (baseEl, presentingEl, duration = 500) => {
+    const backdropAnimation = createAnimation()
+        .addElement(baseEl.querySelector('ion-backdrop'))
+        .fromTo('opacity', 'var(--backdrop-opacity)', 0.0);
+    const wrapperAnimation = createAnimation()
+        .addElement(baseEl.querySelector('.modal-wrapper'))
+        .beforeStyles({ 'opacity': 1 })
+        .fromTo('transform', 'translateY(0%)', 'translateY(100%)');
+    const baseAnimation = createAnimation()
+        .addElement(baseEl)
+        .easing('cubic-bezier(0.32,0.72,0,1)')
+        .duration(duration)
+        .addAnimation([backdropAnimation, wrapperAnimation]);
+    if (presentingEl) {
+        const modalTransform = (presentingEl.tagName === 'ION-MODAL' && presentingEl.presentingElement !== undefined) ? '-10px' : 'max(30px, var(--ion-safe-area-top))';
+        const bodyEl = document.body;
+        const currentPresentingScale = SwipeToCloseDefaults.MIN_PRESENTING_SCALE;
+        const presentingAnimation = createAnimation()
+            .addElement(presentingEl)
+            .beforeClearStyles(['transform'])
+            .afterClearStyles(['transform'])
+            .onFinish(currentStep => {
+            // only reset background color if this is the last card-style modal
+            if (currentStep !== 1) {
+                return;
+            }
+            presentingEl.style.setProperty('overflow', '');
+            const numModals = Array.from(bodyEl.querySelectorAll('ion-modal')).filter(m => m.presentingElement !== undefined).length;
+            if (numModals <= 1) {
+                bodyEl.style.setProperty('background-color', '');
+            }
+        })
+            .keyframes([
+            { offset: 0, filter: 'contrast(0.85)', transform: `translateY(${modalTransform}) scale(${currentPresentingScale})`, borderRadius: '10px 10px 0 0' },
+            { offset: 1, filter: 'contrast(1)', transform: 'translateY(0px) scale(1)', borderRadius: '0px' }
+        ]);
+        baseAnimation.addAnimation(presentingAnimation);
+    }
+    return baseAnimation;
+};
