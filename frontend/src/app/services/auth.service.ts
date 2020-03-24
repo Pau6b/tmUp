79ed2311dda5f4eacb72603aedb3cfa6b798a46d
Daniela Zhao <daniela.zhao4@gmail.com>
2020-03-24 20:41:44 +0100

    AuthService with signup

---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+import { AngularFireAuth } from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  token: string;
+
+  constructor(
+    public afAuth: AngularFireAuth
+  ) { }
+
+  public signUpUser(email: string, password: string): Promise<any> {
+    return this.afAuth.auth.createUserWithEmailAndPassword(email, password);
+  }
+
+}
