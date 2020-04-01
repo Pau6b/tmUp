1c8cde2d255b9fd7af07492516f3b24d07047953
juanjoNA <bubu.08@hotmail.com>
2020-04-01 14:51:38 +0200

    Pantallas sprint 2 + side-menu

---
+++ frontend/src/app/pages/tactics/tactics.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tactics',
+  templateUrl: './tactics.page.html',
+  styleUrls: ['./tactics.page.scss'],
+})
+export class TacticsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
