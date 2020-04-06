ce1caa28e1296518bd6edc886f7239b36556f99a
Clara Sanchez Flaquer <csanchezflaquer@gmail.com>
2020-04-06 10:10:55 +0200

    Google login

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'settings',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
