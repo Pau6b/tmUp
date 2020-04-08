67deb1ed2978aff796ee29dc2c193847a50e4c7c
Clara Sanchez Flaquer <csanchezflaquer@gmail.com>
2020-04-08 12:42:06 +0200

    LogIn Google

--- frontend/src/app/pages/login/login.page.ts
+++ frontend/src/app/pages/login/login.page.ts
@@ -2,12 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActionSheetController, NavController, MenuController } from '@ionic/angular';
 import { FormBuilder, Validators} from '@angular/forms'
 import { AuthService } from 'src/app/services/auth.service';
-import { GooglePlus } from '@ionic-native/google-plus/ngx';
-import { Platform } from '@ionic/angular';
 import * as firebase from 'firebase';
 import { LoadingController } from '@ionic/angular';
-import { Router } from '@angular/router';
-import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
@@ -36,11 +32,7 @@ export class LoginPage implements OnInit {
     public menuCtrl: MenuController,
     public formBuilder: FormBuilder,
     public authService: AuthService,
-    private google: GooglePlus,
-    private platform: Platform,
     public loadingController: LoadingController,
-    private router: Router, 
-    private fireAuth: AngularFireAuth
   ) { }
 
   ngOnInit() {
@@ -65,45 +57,16 @@ export class LoginPage implements OnInit {
     .catch((error:firebase.FirebaseError) => {
       this.logInError=true;
     });
-    
   }
 
-
-  loginGoogle() {
-    let params;
-    if (this.platform.is('android')) {
-      params = {
-        'webClientId': '489608967542-8quc1uc92o0io6f8j1jgmtnban91r3f8.apps.googleusercontent.com',
-        'offline': true
-      }
-    }
-    else {
-      params = {}
-    }
-    this.google.login(params)
-      .then((response) => {
-        const { idToken, accessToken } = response
-        this.onLoginSuccess(idToken, accessToken);
-      }).catch((error) => {
-        console.log(error)
-        alert('error:' + JSON.stringify(error))
-      });
+  logInWithGoogle() {
+    this.authService.loginGoogle()
+    .then(() => {
+      this.navCtrl.navigateRoot('team-list');
+    })
+    .catch((error:firebase.FirebaseError) => {
+      this.logInError=true;
+    });
   }
-  onLoginSuccess(accessToken, accessSecret) {
-    const credential = accessSecret ? firebase.auth.GoogleAuthProvider
-        .credential(accessToken, accessSecret) : firebase.auth.GoogleAuthProvider
-            .credential(accessToken);
-    this.fireAuth.auth.signInWithCredential(credential)
-      .then((response) => {
-        this.router.navigate(["/profile"]);
-        this.loading.dismiss();
-      })
-
-
-  }
-  onLoginError(err) {
-    console.log(err);
-  }
-
 
 }
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
+import { Platform } from '@ionic/angular';
+import * as firebase from 'firebase';
+import { LoadingController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +15,16 @@ export class AuthService {
 
   currentUser;
   error: any;
+  loading: any;
 
   constructor(
     public afAuth: AngularFireAuth,
     public toastCtrl: ToastController,
     private router: Router, 
+    private google: GooglePlus,
+    private platform: Platform,
+    public loadingController: LoadingController,
+    public authService: AuthService
   ) { }
 
   signUpUser(email: string, password: string): Promise<any> {
@@ -27,6 +36,43 @@ export class AuthService {
 
   signIn(email: string, password: string): Promise<any> {
     return this.afAuth.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  //login con google
+  async loginGoogle() {
+    let params;
+    if (this.platform.is('android')) {
+      params = {
+        'webClientId': '489608967542-8quc1uc92o0io6f8j1jgmtnban91r3f8.apps.googleusercontent.com',
+        'offline': true
+      }
+    }
+    else {
+      params = {}
+    }
+    this.google.login(params)
+      .then((response) => {
+        const { idToken, accessToken } = response
+        this.onLoginSuccess(idToken, accessToken);
+      }).catch((error) => {
+        console.log(error)
+        alert('error:' + JSON.stringify(error))
+      });
+  }
+
+  onLoginSuccess(accessToken, accessSecret) {
+    const credential = accessSecret ? firebase.auth.GoogleAuthProvider
+        .credential(accessToken, accessSecret) : firebase.auth.GoogleAuthProvider
+            .credential(accessToken);
+    this.afAuth.auth.signInWithCredential(credential)
+      .then((response) => {
+        this.router.navigate(["/profile"]);
+        this.loading.dismiss();
+      })
+  }
+
+  onLoginError(err) {
+    console.log(err);
   }
 
   //mensaje para indicar el cambio de contraseña
