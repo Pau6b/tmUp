a1eca4d624b5ddbffbb7a54e77121b2ec38e0c1e
Pau6b <50214344+Pau6b@users.noreply.github.com>
2020-04-16 10:19:56 +0200

    Revert "Pau"

--- backend/functions/lib/Teams/Normatives/Normatives.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-/*const admin = require("firebase-admin");
-const db = admin.firestore();*/
-const app = express();
-module.exports = app;
-//# sourceMappingURL=Normatives.js.map
--- backend/functions/src/Teams/Teams.ts
+++ backend/functions/src/Teams/Teams.ts
@@ -1,61 +1,20 @@
 import * as express from 'express';
-import { sports } from '../Core/Core'
-import { GetTeamStatsBySport } from '../Core/Templates/Statistics'
-import { UserRecord } from 'firebase-functions/lib/providers/auth';
 const admin = require("firebase-admin");
 const db = admin.firestore();
 const app = express();
-
 
 //Create => Post
 app.post('/create', (req, res) => {
     (async () => {
         try {
             const jsonContent = JSON.parse(req.body);
-            //Check if the params are correct
-
-            if (req.session!.user == null) {
-                res.status(400).send("T1");
-            }
-
-            let email: any ="";  
-            admin.auth().getUser(req.session!.user).then((user: UserRecord) => {
-                    email = user.email
-            });    
-
-            let errors: string[] = [];
-            let hasErrors: boolean = false;
-            if (!jsonContent.hasOwnProperty("teamName")) {
-                errors.push("TC2");
-                hasErrors = true;
-            }
-            if (!jsonContent.hasOwnProperty("sport")) {
-                errors.push("TC3");
-                hasErrors = true;                
-            }
-            if (!sports.includes(jsonContent.sport)) {
-                errors.push("TC4");
-                hasErrors = true;  
-            }
-            if (hasErrors) {
-                return res.status(400).send(errors);
-            }
-
-            //No errors, we proceed to creation
-            let id = "invalid";
+            let id: any;
             await db.collection('teams').add({
                 teamName: jsonContent.teamName,
-                sport: jsonContent.sport,
-                stats: GetTeamStatsBySport(jsonContent.sport)
+                sport: jsonContent.sport
             }).then((ref:any) => {
                 id= ref.id;
             });
-
-            await db.collection('memberships').add({
-                teamId: id,
-                userId: email,
-                type: "staff"
-            })
             return res.status(200).send(id);
         }
         catch(error){
@@ -68,28 +27,14 @@ app.post('/create', (req, res) => {
 
 
 //Read => Get
-app.get('/:teamId', (req, res) => {
+app.get('/:teamName', (req, res) => {
     (async () => {
         try {
-            const document = db.collection("teams").doc(req.params.teamId);
+            const document = db.collection("teams").doc(req.params.teamName);
+            const user = await document.get();
+            const response = user.data();
 
-            let teamExists:boolean = true;
-            let teamData = await document.get().then((doc: any) => {
-                if(!doc.exists) {
-                    teamExists = false;
-                }
-                else {
-                    return doc.data();
-                }
-            });
-
-            //Check that the user exists
-            if (!teamExists) {
-                return res.status(400).send("Team with teamid : [" + req.params.teamId + "] does not exist");
-            }
-
-            //return correct data
-            return res.status(200).send(teamData);
+            return res.status(200).send(response);
         }
         catch(error){
             console.log(error);
@@ -111,6 +56,7 @@ app.get('/', (req, res) => {
 
                 for (const doc of docs) {
                     const selectedItem  = {
+                        id: doc.data().id,
                         teamName: doc.data().teamName,
                         sport: doc.data().sport
                     };
@@ -131,19 +77,19 @@ app.get('/', (req, res) => {
 
 
 //Update => Put
-app.put('/:teamId', (req, res) => {
+app.put('/:teamName', (req, res) => {
     (async () => {
         try {
             const jsonContent = JSON.parse(req.body);
-        
-            if (!jsonContent.hasOwnProperty("teamName")) {
-                return res.status(400).send("No teamName specified.");
-            }
 
-            await db.collection('teams').doc(req.params.teamId).update({
-                teamName: jsonContent.teamName
+            const document = db.collection('teams').doc(req.params.teamName);
+
+            await document.update({
+                teamName: jsonContent.teamName,
+                sport: jsonContent.sport
             });
             
+
             return res.status(200).send();
         }
         catch(error){
@@ -155,32 +101,13 @@ app.put('/:teamId', (req, res) => {
 });
 
 //Delete => Delete
-/*
 app.delete('/:teamName', (req, res) => {
     (async () => {
         try {
-            let teamExists: boolean = true;
-            const team = db.collection('teams').doc(req.params.teamName).get().then((doc: any) => {
-                if(!doc.exists) {
-                    teamExists = false;
-                }
-            });
-            if (!teamExists) {
-                return res.status(400).send("teamId is incorrect");
-            }
-            await team.delete();
+            const document = db.collection('teams').doc(req.params.teamName);
 
-            const query = db.collectionGroup('memberships').where('teamId',"==",req.params.teamName);
-            const response: any = [];
-
-            await query.get().then((querySnapshot: any) => {
-                const docs = querySnapshot.docs;
-
-                for (const doc of docs) {
-                     doc.delete();
-                }
-                return response;
-            })
+            await document.delete();
+            
             return res.status(200).send();
         }
         catch(error){
@@ -190,5 +117,5 @@ app.delete('/:teamName', (req, res) => {
 
     })().then().catch();
 });
-*/
+
 module.exports = app;
