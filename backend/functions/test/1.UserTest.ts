07df41eb9eb98676e0df28ba66409cf6df82fe73
Ivan <ivan.garcia.sanchez@est.fib.upc.edu>
2020-04-18 20:14:50 +0200

    mas tests

--- backend/functions/test/1.UserTest.ts
+++ backend/functions/test/1.UserTest.ts
@@ -12,7 +12,7 @@ const url = 'http://localhost:5001/tmup-908e4/us-central1/app';
 describe('Create user: ',()=> {
     it('Create user', async() => {
         var user = {
-            "email": "a@a.com",
+            "email": "b@a.com",
             "userName": "b"
         };
         const response = await chaiT.request(url)
@@ -22,10 +22,23 @@ describe('Create user: ',()=> {
         expect(response).to.have.status(200);
             //expect(body).should.include("CM1");
 
-    });
+    }).timeout(5000);
+    it('Create user to delete', async() => {
+        var user = {
+            "email": "delete@a.com",
+            "userName": "b"
+        };
+        const response = await chaiT.request(url)
+        .post('/users/create')
+        .set('content-type', 'application/raw')
+        .send(JSON.stringify(user));
+        expect(response).to.have.status(200);
+            //expect(body).should.include("CM1");
+
+    }).timeout(5000);
     it('Create existing user', async() => {
         var user = {
-            "email": "a@a.com",
+            "email": "b@a.com",
             "userName": "b"
         };
         const response = await chaiT.request(url)
@@ -38,17 +51,29 @@ describe('Create user: ',()=> {
     });
 });
 
-describe('Get user: ',()=> {
-    it('Get user info', async() => {
-        var user = {
-            "email": "a@a.com",
-            "userName": "b"
-        };
+describe('Delete user: ',()=> {
+    it('Delete user info', async() => {
         const response = await chaiT.request(url)
-        .post('/users/create')
-        .set('content-type', 'application/raw')
-        .send(JSON.stringify(user));
+        .delete('/users/delete@a.com');
         expect(response).to.have.status(200);
+            //expect(body).should.include("CM1");
+    });
+});
+
+describe('Get user: ',()=> {
+    it('Get existing user info', async() => {
+        const response = await chaiT.request(url)
+        .get('/users/b@a.com');
+        expect(response).to.have.status(200);
+        expect(response.body.userName).equals("b");
+            //expect(body).should.include("CM1");
+
+    });
+    it('Get unexisting user info', async() => {
+        const response = await chaiT.request(url)
+        .get('/users/delete@a.com');
+        expect(response).to.have.status(200);
+        expect(response.body).empty();
             //expect(body).should.include("CM1");
 
     });
