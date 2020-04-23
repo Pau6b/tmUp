721d8cf9b34420c506ca62dd15e91903b404c727
Lucas <lucas.pinilla@est.fib.upc.edu>
2020-04-23 13:10:11 +0200

    rama lucas

--- backend/functions/lib/Auth/Login.js
+++ backend/functions/lib/Auth/Login.js
@@ -2,25 +2,26 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 //import { UserRecord } from 'firebase-functions/lib/providers/auth';
-//const admin = require("firebase-admin");
+const admin = require("firebase-admin");
 const app = express();
 //Create => Post
 app.post('/', (req, res) => {
     (async () => {
         try {
-            if (req.session.user) {
+            if (req.session.token) {
                 return res.status(400).send("LI2");
             }
-            //const jsonContent = JSON.parse(req.body);
-            const uid = "RVsGf4DOrTbkcvN2mQeoEPYgFoi1";
-            /*admin.auth().verifyIdToken(jsonContent.token).then((decodedToken: any) => {
+            const jsonContent = JSON.parse(req.body);
+            //const uid:any = "RVsGf4DOrTbkcvN2mQeoEPYgFoi1";
+            let uid = "";
+            await admin.auth().verifyIdToken(jsonContent.token).then((decodedToken) => {
                 uid = decodedToken.uid;
-            })*/
+            });
             if (uid === "") {
                 return res.status(400).send("LI1");
             }
             req.session.user = uid;
-            //req.session!.token = jsonContent.token;
+            req.session.token = jsonContent.token;
             return res.status(200).send(req.session.user);
         }
         catch (error) {
