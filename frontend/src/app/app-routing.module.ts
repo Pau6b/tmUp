c33bc3e0180ae9afeedb80200528d24024a2330a
Daniela Zhao <daniela.zhao4@gmail.com>
2020-05-13 19:04:53 +0200

    little changes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -79,11 +79,19 @@ const routes: Routes = [
   {
     path: 'photos',
     loadChildren: () => import('./pages/photos/photos.module').then( m => m.PhotosPageModule)
+  },
+  {
+    path: 'fouls',
+    loadChildren: () => import('./pages/fouls/fouls.module').then( m => m.FoulsPageModule)
+  },
+  {
+    path: 'add-fine',
+    loadChildren: () => import('./pages/add-fine/add-fine.module').then( m => m.AddFinePageModule)
+  },
+  {
+    path: 'statistics',
+    loadChildren: () => import('./pages/statistics/statistics.module').then( m => m.StatisticsPageModule)
   }
-
-
-
-
 ];
 
 @NgModule({
