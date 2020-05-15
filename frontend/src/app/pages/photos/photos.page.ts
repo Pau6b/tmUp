67000f0a7da03a6d7e3ff0ec3356749197c0f144
Clara Sanchez Flaquer <csanchezflaquer@gmail.com>
2020-05-15 12:06:24 +0200

    Menu Photos

--- frontend/src/app/pages/photos/photos.page.ts
+++ frontend/src/app/pages/photos/photos.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { apiRestProvider } from 'src/providers/apiRest/apiRest';
+import { PhotoService } from 'src/app/services/photo.service';
 
 @Component({
   selector: 'app-photos',
@@ -7,6 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotosPage implements OnInit {
 
+  events = [];
+  files = [];
+  hasEvents;
   public data = [
     {
       category: 'Partido Espanyol vs Barça',
@@ -70,10 +75,42 @@ export class PhotosPage implements OnInit {
     centeredSlides: true
   };
 
-  constructor() { }
+  constructor(
+    private apiProv: apiRestProvider,
+    private photoService: PhotoService
+  ) { }
 
   ngOnInit() {
+    this.getEvents();
+    this.getFiles();
   }
 
+  getEvents() {
+    this.apiProv.getEventsOfMonth( new Date().getMonth() )
+    .subscribe( (events) => {
+      this.events = [];
+      let tmp: any;
+      tmp = events;
+      tmp.forEach(element => {
+        let aux = {
+          id: element.id,
+          category: element.title,
+          expanded: true,
+          photos: []
+        }
+        this.events.push(aux);
+      });
+    });
+    if ( this.events.length == 0 ) this.hasEvents = false;
+    else this.hasEvents = true;
+    console.log(this.events);
+  }
+
+  getFiles() {
+    for ( let ev of this.events ) {
+      var p = this.photoService.getFiles('events', this.apiProv.getTeamId(), ev.id, 'event_images');
+      if ( p.length != 0 ) ev.photos = p;
+    }
+  }
 
 }
