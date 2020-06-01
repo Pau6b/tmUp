0369cc9f99ad03e92685c2b20c78d2da656d0147
Juanjo <bubu.8@hotmail.com>
2020-06-01 21:25:27 +0200

    Fisio

--- frontend/src/app/pages/physiotherapist/physiotherapist.page.ts
+++ frontend/src/app/pages/physiotherapist/physiotherapist.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { Validators, FormBuilder } from '@angular/forms';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-physiotherapist',
@@ -8,12 +10,35 @@ import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 })
 export class PhysiotherapistPage implements OnInit {
 
+  role = "physio";
+  pagina = false
   constructor(
-    private iab: InAppBrowser
+    private iab: InAppBrowser,
+    private formBuilder: FormBuilder,
+    private principalPage: AppComponent
   ) { }
 
+  physioForm = this.formBuilder.group({
+    url: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern('www.+[a-zA-Z0-9.-]+.[a-zA-z]{2,4}$')
+      ]
+    ]
+  });
+
   ngOnInit() {
-    window.open('https://www.mundodeportivo.com/', '_system');
+    this.role = this.principalPage.role;
+    if(this.role == 'physio'){
+      //get fisio membership
+    }else{
+      //get all fisios of team
+    }
+  }
+
+  redirect(){
+    this.iab.create("https://calendly.com/cfarre/online?back=1&month=2020-06")
   }
 
 }
