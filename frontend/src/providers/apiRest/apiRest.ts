419c4c9691b368e11b98459010749110549edd4c
Daniela Zhao <daniela.zhao4@gmail.com>
2020-06-03 13:14:21 +0200

    updateProfile works

--- frontend/src/providers/apiRest/apiRest.ts
+++ frontend/src/providers/apiRest/apiRest.ts
@@ -64,7 +64,7 @@ export class apiRestProvider {
   public updateProfileInfo(newName, mail) {
     this.setHeader();
     return new Promise(resolve => {
-      this.http.put(this.url+'users/update', JSON.stringify({userName: newName, email: mail}), { headers: this.headers, responseType:'text' })
+      this.http.put(this.url+'users/update', JSON.stringify({displayName: newName, email: mail}), { headers: this.headers, responseType:'text' })
       .subscribe(data => {
           resolve(data);
         })
